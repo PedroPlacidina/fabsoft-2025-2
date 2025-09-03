546fff0a5c92570a3ef2f975ca77fd53a3901715
Pedro Henrique Placidina <138052698+PedroPlacidina@users.noreply.github.com>
2025-09-03 00:56:01 +0000

    Criação da entity pet

---
+++ fabsoft-backend/src/main/java/br/univille/fabsoft_backend/entity/Pet.java
@@ -0,0 +1,48 @@
+package br.univille.fabsoft_backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String raca;
+    private int idade;
+    private String peco;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getRaca() {
+        return raca;
+    }
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getPeco() {
+        return peco;
+    }
+    public void setPeco(String peco) {
+        this.peco = peco;
+    }
+}
--- fabsoft-backend/src/main/java/br/univille/fabsoft_backend/entity/Usuario.java
+++ fabsoft-backend/src/main/java/br/univille/fabsoft_backend/entity/Usuario.java
@@ -1,10 +1,17 @@
 package br.univille.fabsoft_backend.entity;
 
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.annotation.Generated;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Usuario {
@@ -12,11 +19,22 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @Column(length = 100)
     private String email;
     private String telefone;
     private String senha;
     private String tipo;
 
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+    public Pet getPet() {
+        return pet;
+    }
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
     public String getEmail() {
         return email;
     }

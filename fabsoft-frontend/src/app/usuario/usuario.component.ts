cd56bb65b4fc3a9b536d1431ea89d59170575fa4
Pedro Henrique Placidina <138052698+PedroPlacidina@users.noreply.github.com>
2025-10-15 00:48:44 +0000

    Tabelas

---
+++ fabsoft-frontend/src/app/usuario/usuario.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Usuario } from '../model/usuario';
+import { UsuarioService } from '../service/usuario.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-usuario',
+  imports: [HttpClientModule, CommonModule],
+  templateUrl: './usuario.html',
+  styleUrl: './usuario.css',
+  providers: [UsuarioService]
+})
+export class UsuarioComponent {
+  listaUsuario: Usuario[]=[]
+
+  constructor(private usuarioService: UsuarioService){}
+
+  ngOnInit(){
+    console.log('Pinto enorme.......')
+    this.usuarioService.getUsuarios().subscribe( usuario =>{
+        this.listaUsuario = usuario
+    })
+  }
+
+
+}

2feb484720337b887aee3d9ca58bd7e8ecf8ded6
Pedro Henrique Placidina <138052698+PedroPlacidina@users.noreply.github.com>
2025-10-17 00:41:59 +0000

    Telas ususario

--- fabsoft-frontend/src/app/usuario/usuario.component.ts
+++ fabsoft-frontend/src/app/usuario/usuario.component.ts
@@ -4,24 +4,34 @@ import { UsuarioService } from '../service/usuario.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-usuario',
   imports: [HttpClientModule, CommonModule],
   templateUrl: './usuario.html',
   styleUrl: './usuario.css',
-  providers: [UsuarioService]
+  providers: [UsuarioService, Router]
 })
 export class UsuarioComponent {
   listaUsuario: Usuario[]=[]
 
-  constructor(private usuarioService: UsuarioService){}
+  constructor(
+    private usuarioService: UsuarioService,
+    private router: Router
+    
+    ){}
 
   ngOnInit(){
-    console.log('Pinto enorme.......')
+    console.log('Rodou .......')
     this.usuarioService.getUsuarios().subscribe( usuario =>{
         this.listaUsuario = usuario
     })
+  }
+
+  novo(){
+    this.router.navigate(['usuarios/novo'])
   }
 
 
